2205c1778fea8b9880a11199260d084cf19ae3e0
Rohan_Islam <rohanislam3669@gmail.com>
2024-03-09 01:41:25 +0600

    responsive and Navbar

---
+++ src/components/navbar/Navber.jsx
@@ -0,0 +1,11 @@
+import "./navbar.scss";
+const Navber = () => {
+  return (
+    <nav>
+      <div className="left">left</div>
+      <div className="rigth">rigth</div>
+    </nav>
+  );
+};
+
+export default Navber;
