760944bd5af669e609391aa4d3aa50e296ace037
Rohan_Islam <rohanislam3669@gmail.com>
2024-03-12 00:52:17 +0600

    sm for menu

--- src/components/navbar/Navber.jsx
+++ src/components/navbar/Navber.jsx
@@ -17,6 +17,17 @@ const Navber = () => {
         <a href="" className="register">
           Sign up
         </a>
+        <div className="menuIcon">
+          <img src="/menu.png" alt="" />
+        </div>
+        <div className="menu">
+          <a href="/">Home</a>
+          <a href="/">Abotu</a>
+          <a href="/">Contacts</a>
+          <a href="/">Agents</a>
+          <a href="/">Sign In</a>
+          <a href="/">Sign Up</a>
+        </div>
       </div>
     </nav>
   );
