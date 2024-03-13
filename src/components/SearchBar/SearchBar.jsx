d99c99f7c1a990c277287ce599c0ebbba65df93e
Rohan_Islam <rohanislam3669@gmail.com>
2024-03-13 23:11:35 +0600

    hero section

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,10 @@
+import "./SearchBar.scss";
+const SearchBar = () => {
+  return (
+    <div className="searchBar">
+      <h1>Search Bar</h1>
+    </div>
+  );
+};
+
+export default SearchBar;
--- src/routes/homePage/homePage.jsx
+++ src/routes/homePage/homePage.jsx
@@ -1,8 +1,35 @@
+import SearchBar from "../../components/SearchBar/SearchBar";
 import "./homePage.scss";
 const homePage = () => {
   return (
     <div className="homePage">
-      <div className="textContainer"> text</div>
+      <div className="textContainer">
+        <div className="wrapper">
+          <h1 className="title">Lorem ipsum dolor sit amet</h1>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing Lorem ipsum
+            dolor sit amet consectetur adipisicing elit. Laborum, mollitia!
+            Illum aut repellat alias debitis aperiam inventore iusto incidunt
+            provident mollitia minima magnam earum optio, natus nobis distinctio
+            neque cumque!
+          </p>
+          <SearchBar />
+          <div className="boxes">
+            <div className="box">
+              <h1>16+</h1>
+              <h2>Years of Experience</h2>
+            </div>
+            <div className="box">
+              <h1>200</h1>
+              <h2>Award Gained</h2>
+            </div>
+            <div className="box">
+              <h1>2000+</h1>
+              <h2>Property Ready</h2>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="imgContainer">
         {" "}
         <img src="/bg.png" alt="" />
