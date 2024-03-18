f85a93db909cb49a04b234a5c335dcfcb9d7b9de
Rohan_Islam <rohanislam3669@gmail.com>
2024-03-18 23:57:12 +0600

    react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,35 @@
 import Navber from "./components/navbar/Navber";
 import HomePage from "./routes/homePage/homePage";
+import ListPage from "./routes/listPage/listPage";
 import "./layout.scss";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <HomePage />,
+    },
+    {
+      path: "/list",
+      element: <ListPage />,
+    },
+  ]);
   return (
-    <div className="layout">
-      <div className="navbar">
-        <Navber />
-      </div>
-      <div className="content">
-        {" "}
-        <HomePage />
-      </div>
-    </div>
+    // <div className="layout">
+    //   <div className="navbar">
+    //     <Navber />
+    //   </div>
+    //   <div className="content">
+    //     {" "}
+    //     <HomePage />
+    //   </div>
+    // </div>
+    <RouterProvider router={router} />
   );
 }
 
