c601ebda866fcc380ce31072c0cd09ca458d06c5
Rohan_Islam <rohanislam3669@gmail.com>
2024-03-19 00:18:09 +0600

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,25 @@
-import Navber from "./components/navbar/Navber";
 import HomePage from "./routes/homePage/homePage";
 import ListPage from "./routes/listPage/listPage";
-import "./layout.scss";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Link,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./routes/layout/layout";
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <HomePage />,
-    },
-    {
-      path: "/list",
-      element: <ListPage />,
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <HomePage />,
+        },
+        {
+          path: "/list",
+          element: <ListPage />,
+        },
+      ],
     },
   ]);
-  return (
-    // <div className="layout">
-    //   <div className="navbar">
-    //     <Navber />
-    //   </div>
-    //   <div className="content">
-    //     {" "}
-    //     <HomePage />
-    //   </div>
-    // </div>
-    <RouterProvider router={router} />
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
