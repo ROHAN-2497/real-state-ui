996d6efdfa7a89d817f6c5b6429b922528e7f64d
Rohan_Islam <rohanislam3669@gmail.com>
2024-03-21 01:43:31 +0600

    card

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -10,9 +10,32 @@ const Card = ({ item }) => {
         <img src={item.img} alt="" />
       </Link>
       <div className="textContainer">
-        <h2>
+        <h2 className="title">
           <Link to={`/${item.id}`}>{item.title}</Link>
         </h2>
+        <p className="address">
+          <img src="/pin.png" alt="" />
+          <span>{item.address}</span>
+        </p>
+        <p className="price">${item.price}</p>
+        <div className="bottom">
+          <div className="feature">
+            <img src="/bed.png" alt="" />
+            <span>{item.bedroom} bedroom</span>
+          </div>
+          <div className="feature">
+            <img src="/bed.png" alt="" />
+            <span>{item.bathroom} bathroom</span>
+          </div>
+          <div className="icons">
+            <div className="icon">
+              <img src="/save.png" alt="" />
+            </div>
+            <div className="icon">
+              <img src="/chat.png" alt="" />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
