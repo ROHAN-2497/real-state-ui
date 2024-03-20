1977569d1ea98ca53f461086d9c35923cb9c6bfd
Rohan_Islam <rohanislam3669@gmail.com>
2024-03-21 01:07:02 +0600

    up

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,5 +1,21 @@
-const Card = () => {
-  return <div>Card</div>;
+/* eslint-disable react/prop-types */
+import { Link } from "react-router-dom";
+import "./card.scss";
+
+// eslint-disable-next-line react/prop-types
+const Card = ({ item }) => {
+  return (
+    <div className="card">
+      <Link to={`/${item.id}`} className="imageContainer">
+        <img src={item.img} alt="" />
+      </Link>
+      <div className="textContainer">
+        <h2>
+          <Link to={`/${item.id}`}>{item.title}</Link>
+        </h2>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
