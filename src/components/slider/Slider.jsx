983c24e1e3ec419808776a80715f952ba3f2bee1
Rohan_Islam <rohanislam3669@gmail.com>
2024-03-27 01:59:06 +0600

    up

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,6 +1,18 @@
+/* eslint-disable react/prop-types */
 import "./slider.scss";
-const Slider = () => {
-  return <div className="slider">slider</div>;
+const Slider = ({ images }) => {
+  return (
+    <div className="slider">
+      <div className="bigImage">
+        <img src={images[0]} alt="" />
+      </div>
+      <div className="smallImages">
+        {images.slice(1).map((image, index) => (
+          <img src={images} alt="" key={index} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Slider;
