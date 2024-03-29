8af3a21026001f624d7d2be7d179bb21e9a7dc10
Rohan_Islam <rohanislam3669@gmail.com>
2024-03-30 00:14:41 +0600

    up

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -2,25 +2,27 @@
 import { useState } from "react";
 import "./slider.scss";
 const Slider = ({ images }) => {
-  const { inamgeIndex, setImageIndex } = useState(null);
+  const { inamgeIndex, setImageIndex } = useState(2);
   return (
     <div className="slider">
-      {
+      {inamgeIndex !== null && (
         <div className="fullSlider">
           <div className="arrow">
             <img src="/arrow.png" alt="" />
           </div>
           <div className="imageContainer">
-            <img src={images[0]} alt="" />
+            <img src={images[inamgeIndex]} alt="" />
           </div>
           <div className="arrow">
             <img src="/arrow.png" alt="" className="right" />
           </div>
-          <div className="close">X</div>
+          <div className="close" onClick={() => setImageIndex(null)}>
+            X
+          </div>
         </div>
-      }
+      )}
       <div className="bigImage">
-        <img src={images[0]} alt="" />
+        <img src={images[0]} alt="" onClick={() => setImageIndex(0)} />
       </div>
       <div className="smallImages">
         {images.slice(1).map((image, index) => (
