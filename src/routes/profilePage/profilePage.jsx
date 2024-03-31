8b18acb1022d1ffa8315e21df9ca11e4ef608e99
Rohan_Islam <rohanislam3669@gmail.com>
2024-04-01 03:41:02 +0600

    chat component

--- src/routes/profilePage/profilePage.jsx
+++ src/routes/profilePage/profilePage.jsx
@@ -1,35 +1,39 @@
+import List from "../../components/list/List";
 import "./profilePage.scss";
-const profilePage = () => {
+
+function ProfilePage() {
   return (
     <div className="profilePage">
       <div className="details">
         <div className="wrapper">
           <div className="title">
-            <h1>Useer Information</h1>
+            <h1>User Information</h1>
             <button>Update Profile</button>
-            <div className="info">
-              <span>
-                Avatar:
-                <img
-                  src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-                  alt=""
-                />{" "}
-              </span>
-              <span>
-                UserName : <b>John Doe</b>
-              </span>
-              <span>
-                E-mail : <b>john@gmail.com</b>
-              </span>
-            </div>{" "}
+          </div>
+          <div className="info">
+            <span>
+              Avatar:
+              <img
+                src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                alt=""
+              />
+            </span>
+            <span>
+              Username: <b>John Doe</b>
+            </span>
+            <span>
+              E-mail: <b>john@gmail.com</b>
+            </span>
           </div>
           <div className="title">
             <h1>My List</h1>
-            <button>Add New Post</button>
+            <button>Create New Post</button>
           </div>
+          <List />
           <div className="title">
             <h1>Saved List</h1>
           </div>
+          <List />
         </div>
       </div>
       <div className="chatContainer">
@@ -37,6 +41,6 @@ const profilePage = () => {
       </div>
     </div>
   );
-};
+}
 
-export default profilePage;
+export default ProfilePage;
