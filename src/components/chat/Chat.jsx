a20a8ebadaf78ada3d81ed0d705f64ca758b8acd
Rohan_Islam <rohanislam3669@gmail.com>
2024-04-02 23:58:33 +0600

    up

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -46,8 +46,24 @@ const Chat = () => {
           <span>John Doe</span>
           <p>Lorem ipsum dolor sit amet...</p>
         </div>
+        <div className="message">
+          <img
+            src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+          />{" "}
+          <span>John Doe</span>
+          <p>Lorem ipsum dolor sit amet...</p>
+        </div>
       </div>
-      <div className="chatBox"></div>
+      <div className="chatBox">
+        <div className="top">
+          <div className="user">
+            <img src="" alt="" />
+          </div>
+        </div>
+        <div className="center"></div>
+        <div className="bottom"></div>
+      </div>
     </div>
   );
 };
